e39dae920f84cc031afb1a5e2d8c1c27d58280ae
TvL2386 <drabber2000@gmail.com>
2019-10-17 19:33:15 +0200

    updated version

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,5 @@
 class ResistorColorDuo
-  colors = %w(black brown red orange yellow green blue violet grey white)
-
-  VALUE_MAP = Hash[colors.zip(colors.size.times)]
+  VALUE_MAP = Hash[%w(black brown red orange yellow green blue violet grey white).map.with_index.to_a]
 
   def self.value(colors)
     colors.take(2).map { |color| VALUE_MAP[color] }.join.to_i
