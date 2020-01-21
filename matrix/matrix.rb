074041aa47408a88e87a91e030faf328441a12ae
TvL2386 <drabber2000@gmail.com>
2020-01-21 16:33:20 +0100

    easier solution

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -2,12 +2,10 @@ class Matrix
   attr_reader :rows, :columns
 
   def initialize(text)
-    @rows = text.split("\n").map do |line|
-      line.split(' ').map(&:to_i)
+    @rows = text.lines.map do |line|
+      line.split.map(&:to_i)
     end
 
-    @columns = @rows.count.times.map do |index|
-      @rows.map { |row| row[index] }
-    end
+    @columns = @rows.transpose
   end
 end
